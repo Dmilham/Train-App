e559cfb6ef4fa1919b2985b80f1b3bbb0d0f9a17
Daniel Milham <Stingray@daniels-mbp.fios-router.home>
2016-09-19 11:42:04 -0400

    initial upload

---
+++ trainlogic.js
@@ -0,0 +1,49 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCqr5LhcwBal36rbZs6IW1mP3M39jlWc3A",
+    authDomain: "train-app-9e55d.firebaseapp.com",
+    databaseURL: "https://train-app-9e55d.firebaseio.com",
+    storageBucket: "train-app-9e55d.appspot.com",
+    messagingSenderId: "126052170047"
+  };
+  firebase.initializeApp(config);
+
+// firebase database
+var trainInfo = new Firebase("https://train-app-9e55d.firebaseio.com");
+
+//  Collects input data
+
+var trainName = $("#trainNameInput").val().trim();
+var destination = $("#destinationInput").val().trim();
+var firstTrainMoment = moment($("#firstTrainInput").val().trim());
+var frequency = $("#frequencyInput").val().trim();
+
+// Add a train Button
+$("#addTrainBtn").on("click", function(){
+
+// New object
+var newTrain = {
+		
+	name:  trainName,
+	destination: destination,
+	firstTrain: firstTrainMoment,
+	frequency: frequency
+}
+
+	trainInfo.ref().push(newTrain);
+
+	console.log(newTrain.name);
+	console.log(newTrain.destination); 
+	console.log(firstTrainMoment);
+	console.log(newTrain.frequency)
+
+	$("#trainNameInput").val("");
+	$("#destinationInput").val("");
+	$("#firstTrainInput").val("");
+	$("#frequencyInput").val("");
+
+
+return false;
+
+});
